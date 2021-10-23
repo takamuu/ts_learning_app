e52490e9749459160d1f92d69cdba46a6bf19ba1
takamuu <mr.takamuu@gmail.com>
2021-10-23 10:25:27 +0900

    Function implementatiokn to get from jsonplaceholder(type: any)

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import axios from 'axios';
+import { useState } from 'react';
 import { Practice1 } from './practices/Practice1';
 import { Practice2 } from './practices/Practice2';
 import { Practice3 } from './practices/Practice3';
@@ -5,10 +7,23 @@ import { Practice4 } from './practices/Practice4';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { typeList } from './practices/typeList';
 import './styles.css';
+import { Todo } from './Todo'
 
 export default function App() {
+  const [todos, setTodos] = useState<any>([]);
+
+  const onClickFetchData = () => {
+    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
+      setTodos(res.data);  
+    console.log(res.data);  
+    })
+  }
   return (
     <div className="App">
+      <button onClick={onClickFetchData}>データ取得</button>
+      {todos.map((todo) => (
+        <Todo title={todo.title} userid={todo.userid} />
+      ))}
       <Practice1 />
       <Practice2 />
       <Practice3 />
